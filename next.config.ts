33fd54fa04f80858be32830a58cd335ef707cb4e
developercmit <62554651+developercmit@users.noreply.github.com>
2025-08-27 05:39:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',   // enables static HTML export
-  distDir: 'dist',
+  output: 'export',
 };
 
 export default nextConfig;
